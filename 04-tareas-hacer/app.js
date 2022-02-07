7f1fdb7f835c0cfa9611aab8926d1edb8905d55b
Julio-06 <jtcesarin1998@gmail.com>
2022-02-06 20:58:59 -0500

    04-tareas-hacer

---
+++ 04-tareas-hacer/app.js
@@ -0,0 +1,20 @@
+require('colors');
+
+const { inquirerMenu } = require('./helpers/inquirer');
+
+const main = async () => {
+    console.clear();
+
+    let opt = '';
+
+    do{
+        opt = await inquirerMenu();
+
+        console.log({ opt });
+        
+
+    }while( opt !== '0' );
+
+}
+
+main();
