d4632a7a5cd0b67c39d29e401d0f00fe48a913a8
Julio Tejeira <julio.tejeira@procuraduria.gob.pa>
2022-02-11 11:52:55 -0500

    Models:Tareas, tarea

--- 04-tareas-hacer/app.js
+++ 04-tareas-hacer/app.js
@@ -1,17 +1,26 @@
 require('colors');
 
-const { inquirerMenu } = require('./helpers/inquirer');
+const { inquirerMenu, pausa, leerInput } = require('./helpers/inquirer');
+const Tareas = require('./models/tareas');
 
 const main = async () => {
-    console.clear();
-
     let opt = '';
+    const tareas = new Tareas();
 
     do{
         opt = await inquirerMenu();
 
-        console.log({ opt });
-        
+        switch (opt){
+            case '1':
+                const desc = await leerInput('Descripción:');
+                tareas.crearTarea(desc);
+                break;
+            case '2':
+                console.log(tareas.listadoArr)
+                break;
+        }
+
+        await pausa();
 
     }while( opt !== '0' );
 
